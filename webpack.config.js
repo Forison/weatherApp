29b83e70467b78e7ec886cebfe069b7ad040d9e8
addo <forison6205@gmail.com>
2019-11-27 11:19:05 -0500

    webpack indentation fix2

--- webpack.config.js
+++ webpack.config.js
@@ -14,30 +14,25 @@ module.exports = {
 				test: /\.css$/,
 				use: [
 					'style-loader',
-					'css-loader',
+					'css-loader'
 				]
 			},
 			// image loader
 			{
 				test: /\.(png|svg|jpg|gif)$/,
 				use: [
-					'file-loader'
+					'file-loader',
 				]
 			},
 			// fonts
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'file-loader'
-				]
+				use: ['file-loader']
 			},
 			{
 				test: /\.html$/,
-				use: [
-					'html-loader'
-				]
+				use: ['html-loader']
 			}
-
 		]
 	},
 	devServer: {
